f69a24408b2ca1789f3fee6888fde7875cf2bf30
Mafuyuk <inverter186@gmail.com>
2025-07-17 15:48:02 +0700

    Add files via upload

---
+++ labSQL01_1.sql
@@ -0,0 +1,13 @@
+Select * from Employees
+--
+Select EmployeeID, FirstName, LastName from Employees
+--
+Select * from Employees WHERE City='London'
+--
+Select City,Country  from Customers
+--
+SELECT DISTINCT City, Country FROM Customers
+--
+SELECT * FROM Products WHERE Unitprice > 200
+--
+SELECT * FROM Customers WHERE Country = 'USA' OR City = 'Vancouver'
